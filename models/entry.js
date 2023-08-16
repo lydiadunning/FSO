4b68dd86394f2c4d24b00aec36d264a4e56da298
Lydia Dunning <lydia.dunning@gmail.com>
2023-08-16 16:46:43 -0700

    exercise 3.22

--- models/entry.js
+++ models/entry.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const password = process.env.MONGODB_URI
@@ -8,16 +8,28 @@ const url = `mongodb+srv://mongoUser:${password}@cluster0.xch6lol.mongodb.net/?r
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
-const personSchema = new mongoose.Schema ({
-    name: String,
-    number: String
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number.`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
